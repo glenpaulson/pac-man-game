01847d96823aa3477a853f24f725f752e10a99fb
glenpaulson <glen.gracieux@gmail.com>
2023-05-16 08:36:00 +0530

    chore: project setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector("canvas");
+console.log(canvas);
+
+const c = canvas.getContext("2d");
+console.log(c);
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
