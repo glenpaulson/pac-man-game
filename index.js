b1eb720cbc1c946571b228bbf582e950278166c4
glenpaulson <glen.gracieux@gmail.com>
2023-05-16 09:20:15 +0530

    fix: add boundary class

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const canvas = document.querySelector("canvas");
-console.log(canvas);
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
-const c = canvas.getContext("2d");
-console.log(c);
+canvas.width = innerWidth
+canvas.height = innerHeight
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+class Boundary {
+  constructor({position}){
+    this.position = position
+    this.width = 40
+    this.height = 40
+  }
+
+  draw(){
+    c.fillStyle = 'blue'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+}
+
+const boundary = new Boundary({
+  position: {
+    x: 0,
+    y: 0
+  }
+})
+
+boundary.draw()
+
