e3f534f0290209f34a464a90c955e28bb134b9e7
glenpaulson <glen.gracieux@gmail.com>
2023-05-17 11:46:13 +0530

    fix: add player

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ canvas.width = innerWidth
 canvas.height = innerHeight
 
 class Boundary {
+  static width = 40
+  static height = 40
   constructor({ position }) {
     this.position = position
     this.width = 40
@@ -17,13 +19,45 @@ class Boundary {
   }
 }
 
+class Player {
+  constructor({
+    position, velocity
+  }) {
+    this.position = position
+    this.velocity = velocity
+    this.radius = 10
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+    c.fillStyle = 'yellow'
+    c.fill()
+    c.closePath()
+  }
+}
+
+
+
 const map = [
   ['-', '-', '-', '-', '-', '-'],
   ['-', ' ', ' ', ' ', ' ', '-'],
+  ['-', ' ', '-', '-', ' ', '-'],
   ['-', ' ', ' ', ' ', ' ', '-'],
   ['-', '-', '-', '-', '-', '-']
 ]
 const boundaries = []
+
+const player = new Player({
+  position: {
+    x: 40,
+    y: 40
+  },
+  velocity: {
+    x: 0,
+    y: 0
+  }
+})
 
 map.forEach((row, i) => {
   row.forEach((symbol, j) => {
@@ -31,8 +65,8 @@ map.forEach((row, i) => {
       case '-':
         boundaries.push(new Boundary({
           position: {
-            x: 40 * j,
-            y: 40 * i
+            x: Boundary.width * j,
+            y: Boundary.height * i
           }
         }))
         break
@@ -55,4 +89,4 @@ boundaries.forEach(boundary => {
 
 // boundary.draw()
 
-
+player.draw()
