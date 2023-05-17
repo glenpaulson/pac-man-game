71c14d526d20325e33fd054f1f6483c9cd2ddf43
glenpaulson <glen.gracieux@gmail.com>
2023-05-17 11:30:48 +0530

    fix: create outer-boundary

--- index.js
+++ index.js
@@ -5,24 +5,54 @@ canvas.width = innerWidth
 canvas.height = innerHeight
 
 class Boundary {
-  constructor({position}){
+  constructor({ position }) {
     this.position = position
     this.width = 40
     this.height = 40
   }
 
-  draw(){
+  draw() {
     c.fillStyle = 'blue'
     c.fillRect(this.position.x, this.position.y, this.width, this.height)
   }
 }
 
-const boundary = new Boundary({
-  position: {
-    x: 0,
-    y: 0
-  }
+const map = [
+  ['-', '-', '-', '-', '-', '-'],
+  ['-', ' ', ' ', ' ', ' ', '-'],
+  ['-', ' ', ' ', ' ', ' ', '-'],
+  ['-', '-', '-', '-', '-', '-']
+]
+const boundaries = []
+
+map.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    switch (symbol) {
+      case '-':
+        boundaries.push(new Boundary({
+          position: {
+            x: 40 * j,
+            y: 40 * i
+          }
+        }))
+        break
+
+    }
+    console.log(symbol)
+  })
 })
 
-boundary.draw()
+boundaries.forEach(boundary => {
+  boundary.draw()
+})
+
+// const boundary = new Boundary({
+//   position: {
+//     x: 0,
+//     y: 0
+//   }
+// })
+
+// boundary.draw()
+
 
