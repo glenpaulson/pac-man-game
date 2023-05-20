4a33f706c01b9ceda76b5f962ed8d7be0c1fe159
glenpaulson <glen.gracieux@gmail.com>
2023-05-20 19:31:54 +0530

    fix: update map collision boundaries with imgs
    img assets provided by Chris-Courses

--- index.js
+++ index.js
@@ -7,15 +7,18 @@ canvas.height = innerHeight
 class Boundary {
   static width = 40
   static height = 40
-  constructor({ position }) {
+  constructor({ position, image }) {
     this.position = position
     this.width = 40
     this.height = 40
+    this.image = image
   }
 
   draw() {
-    c.fillStyle = 'blue'
-    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    // c.fillStyle = 'blue'
+    // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+    c.drawImage(this.image, this.position.x, this.position.y)
   }
 }
 
@@ -77,14 +80,22 @@ const keys = {
 let lastKey = ''
 
 const map = [
-  ['-', '-', '-', '-', '-', '-', '-'],
-  ['-', ' ', ' ', ' ', ' ', ' ', '-'],
-  ['-', ' ', '-', ' ', '-', ' ', '-'],
-  ['-', ' ', ' ', ' ', ' ', ' ', '-'],
-  ['-', ' ', '-', ' ', '-', ' ', '-'],
-  ['-', ' ', ' ', ' ', ' ', ' ', '-'],
-  ['-', '-', '-', '-', '-', '-', '-']
+  ['1', '-', '-', '-', '-', '-', '2'],
+  ['|', ' ', ' ', ' ', ' ', ' ', '|'],
+  ['|', ' ', 'b', ' ', 'b', ' ', '|'],
+  ['|', ' ', ' ', ' ', ' ', ' ', '|'],
+  ['|', ' ', 'b', ' ', 'b', ' ', '|'],
+  ['|', ' ', ' ', ' ', ' ', ' ', '|'],
+  ['4', '-', '-', '-', '-', '-', '3']
 ]
+
+function createImg(src) {
+  const image = new Image()
+  image.src = src
+  return image
+}
+
+
 
 map.forEach((row, i) => {
   row.forEach((symbol, j) => {
@@ -94,9 +105,65 @@ map.forEach((row, i) => {
           position: {
             x: Boundary.width * j,
             y: Boundary.height * i
-          }
+          },
+          image: createImg('./img/pipeHorizontal.png')
         }))
         break
+      case '|':
+        boundaries.push(new Boundary({
+          position: {
+            x: Boundary.width * j,
+            y: Boundary.height * i
+          },
+          image: createImg('./img/pipeVertical.png')
+        }))
+        break
+      case '1':
+        boundaries.push(new Boundary({
+          position: {
+            x: Boundary.width * j,
+            y: Boundary.height * i
+          },
+          image: createImg('./img/pipeCorner1.png')
+        }))
+        break
+      case '2':
+        boundaries.push(new Boundary({
+          position: {
+            x: Boundary.width * j,
+            y: Boundary.height * i
+          },
+          image: createImg('./img/pipeCorner2.png')
+        }))
+        break
+      case '3':
+        boundaries.push(new Boundary({
+          position: {
+            x: Boundary.width * j,
+            y: Boundary.height * i
+          },
+          image: createImg('./img/pipeCorner3.png')
+        }))
+        break
+      case '4':
+        boundaries.push(new Boundary({
+          position: {
+            x: Boundary.width * j,
+            y: Boundary.height * i
+          },
+          image: createImg('./img/pipeCorner4.png')
+        }))
+        break
+      case 'b':
+        boundaries.push(new Boundary({
+          position: {
+            x: Boundary.width * j,
+            y: Boundary.height * i
+          },
+          image: createImg('./img/block.png')
+        }))
+        break
+
 
     }
     console.log(symbol)
